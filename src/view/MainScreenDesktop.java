5dba30d1ec7e6ea4010614454f3da96754042618
Ricardo-GTS <ricardo.mat.ci.ufpb@gmail.com>
2023-04-14 20:15:19 -0300

    Next

--- src/view/MainScreenDesktop.java
+++ src/view/MainScreenDesktop.java
@@ -5,94 +5,102 @@ import util.LoginInvalidException;
 import util.PasswordInvalidException;
 
 import java.util.Iterator;
-
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 import business.control.UserManager;
 import business.model.User;
 
 public class MainScreenDesktop {
-	
-	UserManager userManager;
-	
-	public static void main (String[] args) {
-		showMenu();
-	}
-	
-	public static void showMenu() {
-		String option = JOptionPane.showInputDialog("Bem vindo ao sistema GUFPB !\nEscolha a opcao desejada:\n1-Cadastrar Usuario\n2-Listar Usuarios\n3-Excluir Usuario\n4-Sair","Sua opcao");		
-		
-		MainScreenDesktop main = new MainScreenDesktop();
-		
-		main.readUserInput(option);
-	}
-	
-	public void readUserInput(String option) {
-		try {
-			userManager = new UserManager();
-		} catch (InfraException e) {
-			String option2 = JOptionPane.showInputDialog(e.getMessage());		
 
-		}
-		int choice = Integer.parseInt(option);
-		boolean checkedLogin = false;
-		boolean checkedPassword = false;
-		switch (choice) {
-		
-		case 1:
-			
-			while (true) {
-				String name = "";
-				String pass = "";
+    UserManager userManager;
 
-				if (!checkedLogin) {
-					name = JOptionPane.showInputDialog("Nome do usuario:");
-					
-				}
-				if (!checkedPassword) {
-					pass = JOptionPane.showInputDialog("Senha do usuario:");
-					
-				}
+    public static void main(String[] args) {
+        showMenu();
+    }
 
-				try {
-					String [] args = {name, pass};
-					this.userManager.addUser(args);
-					JOptionPane.showMessageDialog(null, "Usuario adicionado com sucesso!");
-					break;
-				} catch (LoginInvalidException e) {
-					JOptionPane.showMessageDialog(null, e.getMessage() );
-					checkedLogin = false;
-					checkedPassword = true;
-				} catch (PasswordInvalidException e) {
-					JOptionPane.showMessageDialog(null, e.getMessage() );
-					checkedLogin = true;
-					checkedPassword = false;
-				}
-				
-			}
-			showMenu();
-			break;
-		
-		case 2:
-			String usuarios = "";
-			Iterator<User> users;
-			try {
-				users = this.userManager.getAllClients().values().iterator();
-				while (users.hasNext()) {
-					User user = users.next();
-					usuarios = usuarios + "[ Login: " + user.getLogin() + " || Senha: " + user.getSenha() + " ]" + "\n";
-				}
-				JOptionPane.showMessageDialog(null, usuarios );
-			} catch (InfraException e) {
-				JOptionPane.showMessageDialog(null, e.getMessage() );
-			}
-			
-			
-			showMenu();
-			break;
-		case 3:
-			break;
-	
-		}
-	}
+    public static void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bem vindo ao sistema GUFPB !");
+        System.out.println("Escolha a opcao desejada:");
+        System.out.println("1-Cadastrar Usuario");
+        System.out.println("2-Listar Usuarios");
+        System.out.println("3-Excluir Usuario");
+        System.out.println("4-Sair");
+
+        String option = scanner.nextLine();
+
+        MainScreenDesktop main = new MainScreenDesktop();
+
+        main.readUserInput(option);
+    }
+
+    public void readUserInput(String option) {
+        try {
+            userManager = new UserManager();
+        } catch (InfraException e) {
+            System.out.println(e.getMessage());
+        }
+        int choice = Integer.parseInt(option);
+        boolean checkedLogin = false;
+        boolean checkedPassword = false;
+        switch (choice) {
+
+            case 1:
+
+                while (true) {
+                    String name = "";
+                    String pass = "";
+
+                    if (!checkedLogin) {
+                        System.out.println("Nome do usuario:");
+                        name = new Scanner(System.in).nextLine();
+
+                    }
+                    if (!checkedPassword) {
+                        System.out.println("Senha do usuario:");
+                        pass = new Scanner(System.in).nextLine();
+
+                    }
+
+                    try {
+                        String[] args = {name, pass};
+                        this.userManager.addUser(args);
+                        System.out.println("Usuario adicionado com sucesso!");
+                        break;
+                    } catch (LoginInvalidException e) {
+                        System.out.println(e.getMessage());
+                        checkedLogin = false;
+                        checkedPassword = true;
+                    } catch (PasswordInvalidException e) {
+                        System.out.println(e.getMessage());
+                        checkedLogin = true;
+                        checkedPassword = false;
+                    }
+
+                }
+                showMenu();
+                break;
+
+            case 2:
+                String usuarios = "";
+                Iterator<User> users;
+                try {
+                    users = this.userManager.getAllClients().values().iterator();
+                    while (users.hasNext()) {
+                        User user = users.next();
+                        usuarios = usuarios + "[ Login: " + user.getLogin() + " || Senha: " + user.getSenha() + " ]" + "\n";
+                    }
+                    System.out.println(usuarios);
+                } catch (InfraException e) {
+                    System.out.println(e.getMessage());
+                }
+
+
+                showMenu();
+                break;
+            case 3:
+                break;
+
+        }
+    }
 }
