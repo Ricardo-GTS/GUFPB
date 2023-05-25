961eb87d47469571ba8115c226094ec137c05687
Ricardo-GTS <ricardo.mat.ci.ufpb@gmail.com>
2023-05-25 10:36:43 -0300

    Factory Method IMplementation

--- src/business/control/UserManagerFacade.java
+++ src/business/control/UserManagerFacade.java
@@ -8,31 +8,33 @@ import java.util.Map;
 import business.model.User;
 
 public class UserManagerFacade {
+    private static UserManagerFacade instance;
+    private UserManager userManager;
 
-	private static UserManagerFacade instance;
-	private UserManager userManager;
+    private UserManagerFacade() throws InfraException {
+        userManager = new UserManager();
+    }
 
-	private UserManagerFacade() throws InfraException {
-		userManager = new UserManager();
-	}
+    public static synchronized UserManagerFacade getInstance() throws InfraException {
+        if (instance == null) {
+            instance = new UserManagerFacade();
+        }
+        return instance;
+    }
 
-	public static synchronized UserManagerFacade getInstance() throws InfraException {
-		if (instance == null) {
-			instance = new UserManagerFacade();
-		}
-		return instance;
-	}
+    public void addUser(String[] args) throws LoginInvalidException, PasswordInvalidException {
+        userManager.addUser(args);
+    }
 
-	public void addUser(String[] args) throws LoginInvalidException, PasswordInvalidException {
-		userManager.addUser(args);
-	}
+    public void removeUser(String login) throws InfraException {
+        userManager.removeUser(login);
+    }
 
-	public void removeUser(String login) throws InfraException {
-		userManager.removeUser(login);
-	}
+    public Map<String, User> getAllClients() throws InfraException {
+        return userManager.getAllClients();
+    }
 
-	public Map<String, User> getAllClients() throws InfraException {
-		return userManager.getAllClients();
-	}
-
+    public UserManager createUserManager() throws InfraException {
+        return new UserManager();
+    }
 }
